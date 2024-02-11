4c7e1b258b336dcd1eb2b70d53d19f0889b3ebdf
sankethsj <sankethjain81@gmail.com>
2024-02-11 15:48:09 +0900

    learn custom traits

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::fmt::{self};
+use std::{fmt, ops::Add};
 
 fn various_prints() {
     println!("Hello, World!");
@@ -69,11 +69,18 @@ fn structures() {
 
     impl fmt::Display for Complex {
         fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            let real_number: f32 = self.real;
+            let mut sign: &str = "+";
+            let mut imaginary: f32 = self.imag;
+            if self.imag < 0.0 {
+                sign = "-";
+                imaginary = imaginary * (-1.0);
+            }
             write!(
                 f,
-                "Complex : {{ 'real': {real}, 'imag': {imag} }}",
-                real = self.real,
-                imag = self.imag
+                "{real} {sign} {imag}i",
+                real = real_number,
+                imag = imaginary
             )
         }
     }
@@ -83,6 +90,70 @@ fn structures() {
         imag: 4.2,
     };
     println!("{}", c);
+
+    impl Add for Complex {
+        type Output = Self;
+
+        fn add(self, rhs: Self) -> Self::Output {
+            Complex {
+                real: self.real + rhs.real,
+                imag: self.imag + rhs.imag,
+            }
+        }
+    }
+
+    let d = Complex {
+        real: -4.5,
+        imag: -7.8,
+    };
+    println!("Complex d : {}", d);
+
+    let c_plus_d = c + d;
+    println!("Adding c and d complex gives : {}", c_plus_d);
+
+    trait CustomTraitWithOptionalTrait {
+        fn print_complex(&self);
+        // this is an optional trait, we can either use in our implementation or not
+        // to make this optional, we have to declare function body and return default value
+        // notice in above trait 'print_complex' we don't have function body, so it is a required implmentation
+        fn get_real(&self) -> f32 {
+            0.0
+        }
+    }
+
+    impl CustomTraitWithOptionalTrait for Complex {
+        fn print_complex(&self) {
+            println!(
+                "Your complex number is :: Real = {real} and imaginary = {imag}",
+                real = self.real,
+                imag = self.imag
+            )
+        }
+    }
+
+    let e: Complex = Complex {
+        real: 6.5,
+        imag: -7.0,
+    };
+    e.print_complex();
+
+    // we can call optional trait methods that are not implemented, it will give us default output 
+    println!("Get real default output : {output}", output=e.get_real());
+
+    // let's have an optional trait, but we implement it
+    trait CustomTraitWithOptionalTraitButWeImplement {
+        fn get_real_now(&self) -> f32 {
+            0.0
+        }
+    }
+
+    impl CustomTraitWithOptionalTraitButWeImplement for Complex {
+        fn get_real_now(&self) -> f32 {
+            self.real
+        }
+    }
+    println!("Get real correct output : {output}", output=e.get_real_now());
+
 }
 
 fn main() {
